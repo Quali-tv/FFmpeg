ff1765292dcd9b9f74f160185d53537057044600
Ernest Wilkerson <ernest.wilkerson@gmail.com>
2021-05-10 00:05:33 -1000

    Silence detection + scene scoring

--- libavfilter/vf_addetect.c
+++ libavfilter/vf_addetect.c
@@ -39,6 +39,7 @@
 #include "scene_sad.h"
 
 #define NUM_AD_DETECT_INFO 1000
+#define MIP_MAP_SIZE 4
 
 typedef struct AdDetectContext {
   const AVClass *class;
@@ -54,15 +55,36 @@ typedef struct AdDetectContext {
   void *koku_ctx;
   int koku_index;
 
-  AVRational time_base;
+  AVRational video_time_base;
+
+  // silence detection
+  double current_silence_duration;
+  double noise;
+  int64_t nb_samples;
+  int nb_channels;
+  int64_t *silence_start;
+  int64_t *silence_duration;
+
+  int64_t frame_end;
+
+  void (*silence_detect)(struct AdDetectContext *s, AVFrame *frame,
+                         int nb_samples, int sample_rate, AVRational time_base);
 
   // scene detection
   int bit_depth;
-  int nb_planes;
-  ptrdiff_t width[4];
-  ptrdiff_t height[4];
+  int linesize;
+  int width;
+  int height;
   ff_scene_sad_fn sad;
-  AVFrame *prev_frame;
+
+  int w, h;
+  AVRational frame_rate;
+  int audio_format, video_format;
+
+  uint8_t *frame;
+  uint8_t *prev_frame;
+  uint8_t *free_frame;
+  uint16_t *acc_frame;
   double prev_mafd;
 } AdDetectContext;
 
@@ -121,48 +143,173 @@ static const AVOption addetect_options[] = {
      0,
      1.0,
      FLAGS},
+    {"noise",
+     "set silence noise tolerance",
+     OFFSET(noise),
+     AV_OPT_TYPE_DOUBLE,
+     {.dbl = 0.01},
+     0,
+     DBL_MAX,
+     FLAGS},
     {NULL}};
 
 AVFILTER_DEFINE_CLASS(addetect);
 
+static av_always_inline void update(AdDetectContext *s, AVFrame *frame,
+                                    int is_silence, int current_sample,
+                                    int sample_rate, AVRational time_base) {
+  int channel = current_sample % s->nb_channels;
+  if (is_silence) {
+    if (s->silence_start[channel] == INT64_MIN) {
+      s->silence_start[channel] =
+          frame->pts + av_rescale_q(current_sample / s->nb_channels + 1,
+                                    (AVRational){1, sample_rate}, time_base);
+      s->silence_duration[channel] = INT64_MIN;
+    } else {
+      const int64_t end_pts =
+          frame ? frame->pts + av_rescale_q(current_sample / s->nb_channels,
+                                            (AVRational){1, sample_rate},
+                                            time_base)
+                : s->frame_end;
+      s->silence_duration[channel] = end_pts - s->silence_start[channel];
+    }
+  } else {
+    s->silence_start[channel] = INT64_MIN;
+    s->silence_duration[channel] = INT64_MIN;
+  }
+}
+
+#define SILENCE_DETECT(name, type)                                           \
+  static void silence_detect_##name(AdDetectContext *s, AVFrame *frame,      \
+                                    int nb_samples, int sample_rate,         \
+                                    AVRational time_base) {                  \
+    const type *p = (const type *)frame->data[0];                            \
+    const type noise = s->noise;                                             \
+    int i;                                                                   \
+                                                                             \
+    for (i = 0; i < nb_samples; i++, p++)                                    \
+      update(s, frame, *p<noise && * p> - noise, i, sample_rate, time_base); \
+  }
+
+SILENCE_DETECT(dbl, double)
+SILENCE_DETECT(flt, float)
+SILENCE_DETECT(s32, int32_t)
+SILENCE_DETECT(s16, int16_t)
+
 static int query_formats(AVFilterContext *ctx) {
   static const enum AVPixelFormat pix_fmts[] = {AV_PIX_FMT_YUV420P,
                                                 AV_PIX_FMT_NONE};
+  static const enum AVSampleFormat sample_fmts[] = {AV_SAMPLE_FMT_FLTP,
+                                                    AV_SAMPLE_FMT_NONE};
+
+  AVFilterLink *inlink = ctx->inputs[0];
+  AVFilterFormats *formats = NULL;
+  AVFilterChannelLayouts *layouts = NULL;
+  int ret = AVERROR(EINVAL);
+
+  formats = ff_make_format_list(sample_fmts);
+  if ((ret = ff_formats_ref(formats, &inlink->outcfg.formats)) < 0 ||
+      (layouts = ff_all_channel_counts()) == NULL ||
+      (ret = ff_channel_layouts_ref(layouts, &inlink->outcfg.channel_layouts)) <
+          0)
+    return ret;
+
+  formats = ff_all_samplerates();
+  if ((ret = ff_formats_ref(formats, &inlink->outcfg.samplerates)) < 0)
+    return ret;
 
   AVFilterFormats *fmts_list = ff_make_format_list(pix_fmts);
   if (!fmts_list) return AVERROR(ENOMEM);
   return ff_set_common_formats(ctx, fmts_list);
 }
 
-static int config_input(AVFilterLink *inlink) {
+static int config_audio_input(AVFilterLink *inlink) {
+  AVFilterContext *ctx = inlink->dst;
+  AdDetectContext *s = ctx->priv;
+
+  s->nb_samples = FFMAX(1, inlink->sample_rate);
+  s->nb_channels = inlink->channels;
+  s->audio_format = inlink->format;
+
+  s->silence_start = av_malloc_array(sizeof(*s->silence_start), s->nb_channels);
+  if (!s->silence_start) return AVERROR(ENOMEM);
+
+  s->silence_duration =
+      av_malloc_array(sizeof(*s->silence_duration), s->nb_channels);
+  if (!s->silence_duration) return AVERROR(ENOMEM);
+
+  for (int c = 0; c < s->nb_channels; c++) {
+    s->silence_start[c] = INT64_MIN;
+    s->silence_duration[c] = INT64_MIN;
+  }
+
+  switch (inlink->format) {
+    case AV_SAMPLE_FMT_DBL:
+      s->silence_detect = silence_detect_dbl;
+      break;
+    case AV_SAMPLE_FMT_FLT:
+    case AV_SAMPLE_FMT_FLTP:
+      s->silence_detect = silence_detect_flt;
+      break;
+    case AV_SAMPLE_FMT_S32:
+      s->noise *= INT32_MAX;
+      s->silence_detect = silence_detect_s32;
+      break;
+    case AV_SAMPLE_FMT_S16:
+      s->noise *= INT16_MAX;
+      s->silence_detect = silence_detect_s16;
+      break;
+    default:
+      av_log(s, AV_LOG_ERROR, "Unknown sample format: %d\n", inlink->format);
+      return AVERROR(ENOMEM);
+  }
+
+  av_log(s, AV_LOG_INFO,
+         "server address: %s, server port: %d, application id: %s, context id: "
+         "%s, width: %d, height: %d, bit depth: %d, black threshold: "
+         "%f, white threshold: %f\n",
+         s->server_address, s->server_port, s->application_id, s->context_id,
+         s->width, s->height, s->bit_depth, s->black_threshold,
+         s->white_threshold);
+
+  return 0;
+}
+
+static int config_video_input(AVFilterLink *inlink) {
   AVFilterContext *ctx = inlink->dst;
   AdDetectContext *s = ctx->priv;
 
   const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(inlink->format);
-  const int is_yuv = !(desc->flags & AV_PIX_FMT_FLAG_RGB) &&
-                     (desc->flags & AV_PIX_FMT_FLAG_PLANAR) &&
-                     desc->nb_components >= 3;
 
   s->koku_index = 0;
   s->bit_depth = desc->comp[0].depth;
-  s->nb_planes = is_yuv ? 1 : av_pix_fmt_count_planes(inlink->format);
   s->black_threshold *= (1 << s->bit_depth);
   s->white_threshold *= (1 << s->bit_depth);
 
-  for (int plane = 0; plane < s->nb_planes; plane++) {
-    const ptrdiff_t line_size =
-        av_image_get_linesize(inlink->format, inlink->w, plane);
-    const int vsub = desc->log2_chroma_h;
+  s->w = inlink->w;
+  s->h = inlink->h;
+  s->frame_rate = inlink->frame_rate;
+  s->video_format = inlink->format;
 
-    s->width[plane] = line_size >> (s->bit_depth > 8);
-    s->height[plane] =
-        plane == 1 || plane == 2 ? AV_CEIL_RSHIFT(inlink->h, vsub) : inlink->h;
-  }
+  s->linesize = av_image_get_linesize(inlink->format, inlink->w, 0);
+  s->width = (s->linesize >> (s->bit_depth > 8)) / (MIP_MAP_SIZE >> 1);
+  s->height = inlink->h / (MIP_MAP_SIZE >> 1);
 
-  s->sad = ff_scene_sad_get_fn(s->bit_depth == 8 ? 8 : 16);
+  s->sad = ff_scene_sad_get_fn(8);
   if (!s->sad) return AVERROR(EINVAL);
 
-  s->time_base = inlink->time_base;
+  s->frame = calloc(s->width * s->height, sizeof(uint8_t));
+  if (!s->frame) return AVERROR(EINVAL);
+
+  s->free_frame = calloc(s->width * s->height, sizeof(uint8_t));
+  if (!s->free_frame) return AVERROR(EINVAL);
+
+  s->acc_frame = calloc(s->width * s->height, sizeof(uint16_t));
+  if (!s->acc_frame) return AVERROR(EINVAL);
+
+  s->prev_frame = NULL;
+
+  s->video_time_base = inlink->time_base;
 
   if (!s->server_address) return AVERROR(EINVAL);
   if (!s->server_port) return AVERROR(EINVAL);
@@ -176,39 +323,32 @@ static int config_input(AVFilterLink *inlink) {
 
   av_log(s, AV_LOG_INFO,
          "server address: %s, server port: %d, application id: %s, context id: "
-         "%s, bit depth: "
-         "%d, nb planes: %d, black threshold: "
+         "%s, width: %d, height: %d, bit depth: %d, black threshold: "
          "%f, white threshold: %f\n",
          s->server_address, s->server_port, s->application_id, s->context_id,
-         s->bit_depth, s->nb_planes, s->black_threshold, s->white_threshold);
+         s->width, s->height, s->bit_depth, s->black_threshold,
+         s->white_threshold);
 
   return 0;
 }
 
-static double get_scene_score(AdDetectContext *s, AVFrame *frame,
-                              AVFrame *prev_frame) {
-  if (prev_frame && frame->height == prev_frame->height &&
-      frame->width == prev_frame->width) {
-    uint64_t sad = 0;
-    uint64_t count = 0;
+static double get_scene_score(AdDetectContext *s, uint8_t *frame,
+                              uint8_t *prev_frame) {
+  uint64_t sad = 0;
+  const int linesize = s->width;
+  const int count = s->width * s->height * (1 << s->bit_depth);
 
-    for (int plane = 0; plane < s->nb_planes; plane++) {
-      uint64_t plane_sad;
+  emms_c();
+  s->sad(prev_frame, linesize, frame, linesize, s->width, s->height, &sad);
 
-      emms_c();
-      s->sad(prev_frame->data[plane], prev_frame->linesize[plane],
-             frame->data[plane], frame->linesize[plane], s->width[plane],
-             s->height[plane], &plane_sad);
-      sad += plane_sad;
-      count += s->width[plane] * s->height[plane];
-    }
-
-    const double mafd = (1.0f * sad) / count;
-    const double diff = fabs(mafd - s->prev_mafd);
-    return av_clipf(FFMIN(mafd, diff) / 100., 0, 1);
+  const double score = (1.0f * sad) / count;
+  if (score > .5) {
+    av_log(s, AV_LOG_INFO,
+           "get_scene_score: %f --> width: %d height: %d sad: %" PRIu64
+           " count: %d\n",
+           score, s->width, s->height, sad, count);
   }
-
-  return 0;
+  return score;
 }
 
 static int get_black_score(const AdDetectContext *s, const uint8_t pixel) {
@@ -219,13 +359,13 @@ static int get_white_score(const AdDetectContext *s, const uint8_t pixel) {
   return pixel >= s->white_threshold;
 }
 
-static double get_pixel_score(const AdDetectContext *s, const AVFrame *frame,
+static double get_pixel_score(const AdDetectContext *s, const uint8_t *frame,
                               const ad_detect_pixel_score_fn score_fn) {
-  const int h = frame->height;
-  const int w = frame->width;
+  const int h = s->height;
+  const int w = s->width;
   uint64_t counter = 0;
 
-  const uint8_t *p = frame->data[0];
+  const uint8_t *p = &frame[0];
   for (int y = 0, pixel_index = 0; y < h; ++y) {
     for (int x = 0; x < w; ++x, ++pixel_index) {
       counter += score_fn(s, p[pixel_index]);
@@ -235,46 +375,107 @@ static double get_pixel_score(const AdDetectContext *s, const AVFrame *frame,
   return (counter * 1.0) / (w * h);
 }
 
-static int filter_frame(AVFilterLink *inlink, AVFrame *frame) {
+static int filter_audio_frame(AVFilterLink *inlink, AVFrame *frame) {
   AVFilterContext *ctx = inlink->dst;
   AdDetectContext *s = ctx->priv;
-  AVFrame *prev_frame = s->prev_frame;
 
-  if (prev_frame) {
-    const double pts = frame->pts * av_q2d(s->time_base);
-    const double scene_score = get_scene_score(s, frame, prev_frame);
-    const double black_score = get_pixel_score(s, frame, get_black_score);
-    const double white_score = get_pixel_score(s, frame, get_white_score);
+  const int nb_channels = inlink->channels;
+  const int srate = inlink->sample_rate;
+  const int nb_samples = frame->nb_samples * nb_channels;
+
+  s->frame_end =
+      frame->pts + av_rescale_q(frame->nb_samples, (AVRational){1, srate},
+                                inlink->time_base);
+
+  s->silence_detect(s, frame, nb_samples, srate, inlink->time_base);
+
+  int64_t silence_duration = INT64_MAX;
+  for (int c = 0; c < s->nb_channels; ++c) {
+    silence_duration = FFMIN(silence_duration, s->silence_duration[c]);
+  }
+
+  if (silence_duration == INT64_MIN) {
+    s->current_silence_duration = 0.0;
+  } else {
+    s->current_silence_duration = silence_duration * av_q2d(inlink->time_base);
+  }
+
+  if (ctx->nb_outputs > 0) {
+    return ff_filter_frame(ctx->outputs[0], frame);
+  } else {
+    av_frame_free(&frame);
+    return 0;
+  }
+}
+
+static int filter_video_frame(AVFilterLink *inlink, AVFrame *frame) {
+  AVFilterContext *ctx = inlink->dst;
+  AdDetectContext *s = ctx->priv;
+
+  {
+    const int h = frame->height;
+    const int w = frame->width;
+    const int count = s->width * s->height;
+    uint8_t *src_p = &frame->data[0][0];
+    uint16_t *dest_p = &s->acc_frame[0];
+
+    memset(dest_p, 0, count * sizeof(uint16_t));
+    for (int y = 0, index = 0; y < h; ++y) {
+      for (int x = 0; x < w; ++x, ++src_p, ++index) {
+        dest_p[index / MIP_MAP_SIZE] += *src_p;
+      }
+    }
+
+    for (int i = 0; i < count; ++i) {
+      s->frame[i] = dest_p[i] / MIP_MAP_SIZE;
+    }
+  }
+
+  if (s->prev_frame) {
+    const double pts = frame->pts * av_q2d(s->video_time_base);
+    const double scene_score = get_scene_score(s, s->frame, s->prev_frame);
+    const double black_score = get_pixel_score(s, s->frame, get_black_score);
+    const double white_score = get_pixel_score(s, s->frame, get_white_score);
 
     Koku_add_scene_detection_info(s->koku_ctx, pts, scene_score, black_score,
-                                  white_score);
+                                  white_score, s->current_silence_duration);
 
     if ((++s->koku_index % NUM_AD_DETECT_INFO) == 0) {
       Koku_transmit_scene_detection_info(s->koku_ctx);
     }
 
-    av_frame_free(&prev_frame);
+    s->free_frame = s->prev_frame;
   }
 
-  s->prev_frame = av_frame_clone(frame);
-  return ff_filter_frame(inlink->dst->outputs[0], frame);
+  s->prev_frame = s->frame;
+  s->frame = s->free_frame;
+
+  if (ctx->nb_outputs > 0) {
+    return ff_filter_frame(ctx->outputs[0], frame);
+  } else {
+    av_frame_free(&frame);
+    return 0;
+  }
 }
 
 static void addetect_uninit(AVFilterContext *ctx) {}
 
-static const AVFilterPad addetect_inputs[] = {{
-                                                  .name = "default",
-                                                  .type = AVMEDIA_TYPE_VIDEO,
-                                                  .config_props = config_input,
-                                                  .filter_frame = filter_frame,
-                                              },
-                                              {NULL}};
+static const AVFilterPad addetect_inputs[] = {
+    {
+        .name = "default_audio",
+        .type = AVMEDIA_TYPE_AUDIO,
+        .config_props = config_audio_input,
+        .filter_frame = filter_audio_frame,
+    },
+    {
+        .name = "default_video",
+        .type = AVMEDIA_TYPE_VIDEO,
+        .config_props = config_video_input,
+        .filter_frame = filter_video_frame,
+    },
+    {NULL}};
 
-static const AVFilterPad addetect_outputs[] = {{
-                                                   .name = "default",
-                                                   .type = AVMEDIA_TYPE_VIDEO,
-                                               },
-                                               {NULL}};
+static const AVFilterPad addetect_outputs[] = {{NULL}};
 
 AVFilter ff_vf_addetect = {
     .name = "addetect",
--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-make -j 8 && ./ffmpeg -y -i ~/cctv1.mp4 -vf "addetect=server_address=localhost:server_port=5000:application_id=test_app:context_id=test_context" -f null -
+make -j 8 && ./ffmpeg -y -i ~/bbb.webm -filter_complex "[0:a][0:v]addetect=server_address=api.imernest.com:server_port=8000:application_id=spotz:context_id=testz" -f null -
