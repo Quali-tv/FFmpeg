0fe0af9f3ceb8e236c29737b7b37ee3274c74667
Ernest Wilkerson <ernest.wilkerson@gmail.com>
2021-05-30 11:50:41 -0700

    Add audio levels

--- libavfilter/vf_addetect.c
+++ libavfilter/vf_addetect.c
@@ -41,6 +41,14 @@
 #define NUM_AD_DETECT_INFO 1000
 #define MIP_MAP_SIZE 4
 
+typedef struct AudioLevelContext {
+  long count;
+  double mean;
+  double m2;
+  double delta;
+  double delta2;
+} AudioLevelContext;
+
 typedef struct AdDetectContext {
   const AVClass *class;
 
@@ -48,6 +56,7 @@ typedef struct AdDetectContext {
   const char *server_address;
   int server_port;
   const char *application_id;
+  const char *session_id;
   const char *context_id;
   int overwrite_existing_data;
   double black_threshold;
@@ -63,6 +72,10 @@ typedef struct AdDetectContext {
   double noise;
   int64_t nb_samples;
   int nb_channels;
+
+  AudioLevelContext *audio_levels;
+  int audio_levels_need_reset;
+
   int64_t *silence_start;
   int64_t *silence_duration;
 
@@ -120,6 +133,14 @@ static const AVOption addetect_options[] = {
      0,
      0,
      FLAGS},
+    {"session_id",
+     "set koku session id",
+     OFFSET(session_id),
+     AV_OPT_TYPE_STRING,
+     {.str = NULL},
+     0,
+     0,
+     FLAGS},
     {"context_id",
      "set koku context id",
      OFFSET(context_id),
@@ -128,7 +149,7 @@ static const AVOption addetect_options[] = {
      0,
      0,
      FLAGS},
-     {"overwrite_existing_data",
+    {"overwrite_existing_data",
      "overwrite existing data if found",
      OFFSET(overwrite_existing_data),
      AV_OPT_TYPE_BOOL,
@@ -164,9 +185,33 @@ static const AVOption addetect_options[] = {
 
 AVFILTER_DEFINE_CLASS(addetect);
 
-static av_always_inline void update(AdDetectContext *s, AVFrame *frame,
-                                    int is_silence, int current_sample,
-                                    int sample_rate, AVRational time_base) {
+static av_always_inline void audio_level_reset(AdDetectContext *s) {
+  for (int i = 0; i < s->nb_channels; ++i) {
+    AudioLevelContext *a = &s->audio_levels[i];
+    a->count = 0;
+    a->delta = 0;
+    a->delta2 = 0;
+    a->mean = 0;
+    a->m2 = 0;
+  }
+}
+
+static av_always_inline void audio_level_update(AdDetectContext *s,
+                                                const double sample,
+                                                const int current_sample) {
+  int channel = current_sample % s->nb_channels;
+  AudioLevelContext *a = &s->audio_levels[channel];
+  ++a->count;
+  a->delta = sample - a->mean;
+  a->mean += a->delta / a->count;
+  a->delta2 = sample - a->mean;
+  a->m2 += a->delta * a->delta2;
+}
+
+static av_always_inline void silence_update(AdDetectContext *s, AVFrame *frame,
+                                            int is_silence, int current_sample,
+                                            int sample_rate,
+                                            AVRational time_base) {
   int channel = current_sample % s->nb_channels;
   if (is_silence) {
     if (s->silence_start[channel] == INT64_MIN) {
@@ -188,16 +233,24 @@ static av_always_inline void update(AdDetectContext *s, AVFrame *frame,
   }
 }
 
-#define SILENCE_DETECT(name, type)                                           \
-  static void silence_detect_##name(AdDetectContext *s, AVFrame *frame,      \
-                                    int nb_samples, int sample_rate,         \
-                                    AVRational time_base) {                  \
-    const type *p = (const type *)frame->data[0];                            \
-    const type noise = s->noise;                                             \
-    int i;                                                                   \
-                                                                             \
-    for (i = 0; i < nb_samples; i++, p++)                                    \
-      update(s, frame, *p<noise && * p> - noise, i, sample_rate, time_base); \
+#define SILENCE_DETECT(name, type)                                       \
+  static void silence_detect_##name(AdDetectContext *s, AVFrame *frame,  \
+                                    int nb_samples, int sample_rate,     \
+                                    AVRational time_base) {              \
+    const type *p = (const type *)frame->data[0];                        \
+    const type noise = s->noise;                                         \
+    int i;                                                               \
+                                                                         \
+    if (s->audio_levels_need_reset) {                                    \
+      audio_level_reset(s);                                              \
+      s->audio_levels_need_reset = 0;                                    \
+    }                                                                    \
+                                                                         \
+    for (i = 0; i < nb_samples; i++, p++) {                              \
+      audio_level_update(s, (double)*p, i);                              \
+      silence_update(s, frame, *p<noise && * p> - noise, i, sample_rate, \
+                     time_base);                                         \
+    }                                                                    \
   }
 
 SILENCE_DETECT(dbl, double)
@@ -240,6 +293,9 @@ static int config_audio_input(AVFilterLink *inlink) {
   s->nb_channels = inlink->channels;
   s->audio_format = inlink->format;
 
+  s->audio_levels = av_malloc_array(sizeof(*s->audio_levels), s->nb_channels);
+  if (!s->audio_levels) return AVERROR(ENOMEM);
+
   s->silence_start = av_malloc_array(sizeof(*s->silence_start), s->nb_channels);
   if (!s->silence_start) return AVERROR(ENOMEM);
 
@@ -274,12 +330,14 @@ static int config_audio_input(AVFilterLink *inlink) {
   }
 
   av_log(s, AV_LOG_INFO,
-         "server address: %s, server port: %d, application id: %s, context id: "
-         "%s, overwrite: %d, width: %d, height: %d, bit depth: %d, black threshold: "
+         "server address: %s, server port: %d, application id: %s, session id: "
+         "%s, context id: "
+         "%s, overwrite: %d, width: %d, height: %d, bit depth: %d, black "
+         "threshold: "
          "%f, white threshold: %f\n",
-         s->server_address, s->server_port, s->application_id, s->context_id, s->overwrite_existing_data,
-         s->width, s->height, s->bit_depth, s->black_threshold,
-         s->white_threshold);
+         s->server_address, s->server_port, s->application_id, s->session_id,
+         s->context_id, s->overwrite_existing_data, s->width, s->height,
+         s->bit_depth, s->black_threshold, s->white_threshold);
 
   return 0;
 }
@@ -323,20 +381,24 @@ static int config_video_input(AVFilterLink *inlink) {
   if (!s->server_address) return AVERROR(EINVAL);
   if (!s->server_port) return AVERROR(EINVAL);
   if (!s->application_id) return AVERROR(EINVAL);
+  if (!s->session_id) return AVERROR(EINVAL);
   if (!s->context_id) return AVERROR(EINVAL);
 
-  s->koku_ctx = Koku_create(s->server_address, s->server_port,
-                            s->application_id, s->context_id,s->overwrite_existing_data);
+  s->koku_ctx =
+      Koku_create(s->server_address, s->server_port, s->application_id,
+                  s->session_id, s->context_id, s->overwrite_existing_data);
 
   if (!s->koku_ctx) return AVERROR(EINVAL);
 
   av_log(s, AV_LOG_INFO,
-         "server address: %s, server port: %d, application id: %s, context id: "
-         "%s, overwrite: %d, width: %d, height: %d, bit depth: %d, black threshold: "
+         "server address: %s, server port: %d, application id: %s, session id: "
+         "%s, context id: "
+         "%s, overwrite: %d, width: %d, height: %d, bit depth: %d, black "
+         "threshold: "
          "%f, white threshold: %f\n",
-         s->server_address, s->server_port, s->application_id, s->context_id, s->overwrite_existing_data,
-         s->width, s->height, s->bit_depth, s->black_threshold,
-         s->white_threshold);
+         s->server_address, s->server_port, s->application_id, s->session_id,
+         s->context_id, s->overwrite_existing_data, s->width, s->height,
+         s->bit_depth, s->black_threshold, s->white_threshold);
 
   return 0;
 }
@@ -418,6 +480,7 @@ static int filter_audio_frame(AVFilterLink *inlink, AVFrame *frame) {
 }
 
 static int filter_video_frame(AVFilterLink *inlink, AVFrame *frame) {
+  static double audio_levels[32];
   AVFilterContext *ctx = inlink->dst;
   AdDetectContext *s = ctx->priv;
 
@@ -446,8 +509,15 @@ static int filter_video_frame(AVFilterLink *inlink, AVFrame *frame) {
     const double black_score = get_pixel_score(s, s->frame, get_black_score);
     const double white_score = get_pixel_score(s, s->frame, get_white_score);
 
+    for (int i = 0; i < s->nb_channels; ++i) {
+      audio_levels[i] = s->audio_levels[i].mean;
+    }
+
     Koku_add_scene_detection_info(s->koku_ctx, pts, scene_score, black_score,
-                                  white_score, s->current_silence_duration);
+                                  white_score, s->current_silence_duration,
+                                  audio_levels, s->nb_channels);
+
+    s->audio_levels_need_reset = 1;
 
     if ((++s->koku_index % NUM_AD_DETECT_INFO) == 0) {
       Koku_transmit_scene_detection_info(s->koku_ctx);
